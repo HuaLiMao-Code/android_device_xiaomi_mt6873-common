91c85f64b65f2fde01c9a20693c5cdeba7e53e15
HuaLiMao-AQ <hualimao_aq@163.com>
2022-02-13 19:00:16 +0800

    Initial atom tree Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

---
+++ device.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2022 The MiKu-UI Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 29 30
