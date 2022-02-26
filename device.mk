2ae13ce1dcea08ffb7e9a18222914d9313582e8d
HuaLiMao-AQ <hualimao_aq@163.com>
2022-02-27 00:29:42 +0800

    atom: Build Ims Service Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- device.mk
+++ device.mk
@@ -204,6 +204,15 @@ PRODUCT_PACKAGES += \
     RcsService
 
 # Telephony
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common \
+
 PRODUCT_PACKAGES += \
     libshim_vtservice \
     ImsServiceBase
